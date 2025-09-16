6261be44d40b961360ced85c9a92bb28f15acccc
JustRun_0822 <63898236+justrun0822@users.noreply.github.com>
2025-09-16 11:01:32 +0800

    Update __init__.py
    修改  __init__.py 文件，删除多余的引入。

--- my_models/__init__.py
+++ my_models/__init__.py
@@ -1,16 +1,9 @@
-from .my_iqa import (
-    MyIQAModel, CNNBaselineModel, ViTBaselineModel, 
-    CNNViTNoFusionModel, CNNViTWaveletModel
-)
+from .my_iqa import MyIQAModel
 from .backbones import ResNet50, VisionTransformer
 from .components import AFPN, Fusion, BasicConv, BasicBlock
 
 __all__ = [
     'MyIQAModel',
-    'CNNBaselineModel',
-    'ViTBaselineModel', 
-    'CNNViTNoFusionModel',
-    'CNNViTWaveletModel',
     'ResNet50',
     'VisionTransformer',
     'AFPN',
